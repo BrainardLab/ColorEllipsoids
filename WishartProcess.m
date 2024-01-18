8b87b69a3dac271eda689a4ba6a4172742b3f2f4
FangfangHong <fh862@nyu.edu>
2024-01-18 14:32:11 -0500

    Cleaned up the code

--- WishartProcess.m
+++ WishartProcess.m
@@ -1,8 +1,8 @@
 clear all; close all; clc; 
 %define some parameters
 global MAX_DEGREE NUM_DIMS EXTRA_DIMS NUM_GRID_PTS NUM_MC_SAMPLES
-MAX_DEGREE     = 5;
-NUM_DIMS       = 2;
+MAX_DEGREE     = 5;     %corresponds to p in Alex's document
+NUM_DIMS       = 2;     %corresponds to a = 1, a = 2 
 EXTRA_DIMS     = 1;
 NUM_GRID_PTS   = 28;
 NUM_MC_SAMPLES = 100;
@@ -35,54 +35,7 @@ title('Chebyshev polynomials of the first kind')
 % T(1)T(0), T(1)T(1), ... , T(1)T(4);
 % ...       ...       ...   ...
 % T(4)T(0), T(4)T(1), ... , T(4)T(4)];
-[XG, YG]    = meshgrid(xg, yg);
-M_chebyshev = NaN(MAX_DEGREE,MAX_DEGREE,NUM_MC_SAMPLES,NUM_MC_SAMPLES);
-for i = 1:MAX_DEGREE
-    %grab the function handle
-    Ti_func = matlabFunction(T(i));
-    %T(0) = @()1.0 
-    %it doesn't take any input arguments, so let's make it a special case
-    if i==1; Ti = ones(size(XG)); else; Ti = Ti_func(XG); end
-    for j = 1:MAX_DEGREE
-        Tj_func = matlabFunction(T(j));
-        if j == 1; Tj = ones(size(YG)); else; Tj = Tj_func(YG); end
-        M_chebyshev(i,j,:,:) = Ti.*Tj;
-    end
-end
 
-%visualize it
-figure
-for i = 1:MAX_DEGREE
-    for j = 1:MAX_DEGREE
-        subplot(MAX_DEGREE,MAX_DEGREE,j+(i-1)*5); colormap(summer)
-        imagesc(squeeze(M_chebyshev(i,j,:,:)));
-        xticks([]); yticks([]); axis square;
-    end
-end
-
-%% randomly generate linear combinations of the basis function
-%try 4 instances
-nInstances = 4;
-%draw values from Gaussian distributions and use them as weights for the
-%basis functions
-w = randn(MAX_DEGREE,MAX_DEGREE,nInstances);
-M_chebyshev_rand = NaN(nInstances,NUM_MC_SAMPLES,NUM_MC_SAMPLES);
-for n = 1:nInstances
-    %size of w_rep: MAX_DEGREE x MAX_DEGREE x NUM_MC_SAMPLES x NUM_MC_SAMPLES
-    %               (5 x 5 x 100 x 100)
-    w_rep = repmat(w(:,:,n),[1,1,NUM_MC_SAMPLES,NUM_MC_SAMPLES]);
-    %sum all the 2d basis funcitons (across the 1st and 2nd dimensions)
-    M_chebyshev_rand(n,:,:) = sum(sum(M_chebyshev.*w_rep,1),2);
-end
-
-figure
-for n = 1:nInstances
-    subplot(1,nInstances,n); colormap(summer)
-    imagesc(squeeze(M_chebyshev_rand(n,:,:)));
-    xticks([]); yticks([]); axis square;
-end
-
-%% PART2: Sampling from the Wishart Process prior
 %get the polynomial coefficients of the basis function
 %T(0) = 1               -> [0, 0, 0, 0, 1] corresponds to coefficients to [x^5, x^4, x^3, x^2, x^1, x^0]
 %T(1) = x               -> [0, 0, 0, 1, 0]
@@ -92,64 +45,58 @@ end
 Basis_chebyshev = compute_chebyshev_basis_coeffs;
 disp(Basis_chebyshev)
 
+[XG, YG]    = meshgrid(xg, yg);
+[~, M_chebyshev] = compute_U(Basis_chebyshev,[],XG,YG);
+
+%visualize it
+plot_heatmap(M_chebyshev)
+
+%% randomly generate linear combinations of the basis function
+%try 4 instances
+nInstances = 4;
+%draw values from Gaussian distributions and use them as weights for the
+%basis functions
+w = randn(MAX_DEGREE,MAX_DEGREE,nInstances,1);
+M_chebyshev_rand = compute_U(Basis_chebyshev, w, XG, YG);
+
+%visualize it
+plot_heatmap(M_chebyshev_rand)
+
+%% PART2: Sampling from the Wishart Process prior
 %Sample W. As the degree of polynomial increases, the std of weights
 %decreases. 
 [W_true, logprior_W] = sample_W_prior(VARIANCE_SCALE, DECAY_RATE);
 
 %For debugging, load W_true.m exported from Alex's code. This way, we can
 %test if the code below works as expected.
-clear W_true %comment it out after debugging
+clear W_true                 %comment it out after debugging
 load('W_true.mat','W_true')
 
 %Compute U, which is essentially the weighted sum of basis functions
-[XT, XV] = meshgrid(linspace(-1,1, NUM_GRID_PTS), linspace(-1,1, NUM_GRID_PTS));
-[U_true,Phi] = compute_U(Basis_chebyshev, W_true, XT, XV);
+[XT, YT] = meshgrid(linspace(-1,1, NUM_GRID_PTS), linspace(-1,1, NUM_GRID_PTS));
+[U_true,Phi] = compute_U(Basis_chebyshev, W_true, XT, YT);
+
 %Sanity check: if I didn't mess up with dimensions, then the following
 %figure should look identical to the ones generated before (except the
 %sampling is coarser) 
-figure
-for i = 1:MAX_DEGREE
-    for j = 1:MAX_DEGREE
-        subplot(MAX_DEGREE,MAX_DEGREE,j+(i-1)*5); colormap(summer)
-        imagesc(squeeze(Phi(:,:,i,j)));
-        xticks([]); yticks([]); axis square;
-    end
-end
+%visualize it
+plot_heatmap(Phi)
 
 %also visualize U. It shouldn't be that much different than the ones 
 %randomly generated before with randomly sampled weights
-figure
-for i = 1:NUM_DIMS
-    for j = 1:(NUM_DIMS+EXTRA_DIMS)
-        subplot(NUM_DIMS, NUM_DIMS+EXTRA_DIMS, j+(i-1)*(NUM_DIMS+EXTRA_DIMS))
-        colormap(summer)
-        imagesc(squeeze(U_true(:,:,i,j))); 
-        xticks([]); yticks([]); axis square;
-    end
-end
+plot_heatmap(U_true)
 
 %% At the end, compute Sigmas_true given U
 %the following code is equivalent to 
 %Sigmas_true = jnp.einsum("ijdv,ijev->ijde", U_true, U_true) in python
 %but in MATLAB tensorprod.m doesn't allow that operation, so I had to do it
 %in a tedious way
-
-% Reshape U for the operation, aligning the dimensions for multiplication
-U1 = reshape(U_true, [], NUM_DIMS, NUM_DIMS+EXTRA_DIMS);
-U2 = reshape(U_true, [], NUM_DIMS, NUM_DIMS+EXTRA_DIMS);
-
-% Perform the tensor contraction
-% Multiplying and summing over the last dimension of U1 and the fourth dimension of U2
-C = zeros(size(U1, 1), size(U1, 2), size(U2, 2));
-for i = 1:size(U1, 2)
-    for j = 1:size(U2, 2)
-        C(:, i, j) = sum(U1(:, i, :) .* U2(:, j, :), 3);
+Sigmas_true = NaN(NUM_GRID_PTS, NUM_GRID_PTS, NUM_DIMS, NUM_DIMS);
+for i = 1:NUM_DIMS
+    for j = 1:NUM_DIMS
+        Sigmas_true(:,:,i,j) = sum(U_true(:,:,i,:).*U_true(:,:,j,:),4);
     end
 end
-
-% Reshape C back to the desired 4D format (NUM_GRID_PTS x NUM_GRID_PTS x 2 x 2)
-Sigmas_true = reshape(C, size(U_true, 1), size(U_true, 2),...
-    size(U_true, 3), size(U_true, 3));
 
 % visualize
 thetas = linspace(0,2*pi, 50);
@@ -158,14 +105,14 @@ figure
 for i = 1:2:NUM_GRID_PTS
     for j = 1:2:NUM_GRID_PTS
         sig_ij = sqrtm(squeeze(Sigmas_true(i,j,:,:)))*sinusoids;
-        plot(sig_ij(1,:)+XT(i,j), sig_ij(2,:)+XV(i,j),'k'); hold on
+        plot(sig_ij(1,:)+XT(i,j), sig_ij(2,:)+YT(i,j),'k'); hold on
     end
 end
 title('Sample from Wishart process prior');
 
 %Questions so far:
 %1. what does the extra dimension represent? 
-%2. Why is sigma = sqrt(U*U')?
+%2. the scaler for variance is not expressed in the function of phi
 
 %% Part 3: Predicting the probability of error from the model
 % %simulate eta
@@ -180,7 +127,7 @@ predict_error_prob(zeros(1,1,NUM_DIMS), ones(1,1,NUM_DIMS),...
 
 %let's set a grid for x and xbar
 XBAR = NaN(NUM_GRID_PTS, NUM_GRID_PTS, 2);
-XBAR(:,:,1) = XT; XBAR(:,:,2) = XV;
+XBAR(:,:,1) = XT; XBAR(:,:,2) = YT;
 nX = 4;
 [XVAL_1, XVAL_2] = meshgrid(linspace(-1,1,nX), linspace(-1,1,nX));
 probs = NaN(nX,nX, NUM_GRID_PTS, NUM_GRID_PTS);
@@ -232,23 +179,31 @@ function [W, sum_logpriorW] = sample_W_prior(var_scale, decay_rate)
     sum_logpriorW = sum(logpriorW(:));
 end
 
-function [U, phi] = compute_U(poly_chebyshev, W,xt,xv)
+function [U, phi] = compute_U(poly_chebyshev, W,xt,yt)
     global MAX_DEGREE 
     NUM_GRID_PTS = size(xt,1);
-    [val_xt, val_xv] = deal(NaN(NUM_GRID_PTS, NUM_GRID_PTS, MAX_DEGREE));
+    [val_xt, val_yt] = deal(NaN(NUM_GRID_PTS, NUM_GRID_PTS, MAX_DEGREE));
     for d = 1:MAX_DEGREE 
         val_xt(:,:,d) = polyval(poly_chebyshev(d,:),xt);
-        val_xv(:,:,d) = polyval(poly_chebyshev(d,:),xv);
+        val_yt(:,:,d) = polyval(poly_chebyshev(d,:),yt);
     end
-    val_xxt = repmat(val_xt, [1,1,1,size(poly_chebyshev,2)]);
-    val_xxv = permute(repmat(val_xv, [1,1,1,size(poly_chebyshev,2)]),[1,2,4,3]);
-    % val_xxv = repmat(val_xv, [1,1,1,size(poly_chebyshev,2)]);
+
+    val_xt_repmat = repmat(val_xt, [1,1,1,size(poly_chebyshev,2)]);
+    val_yt_repmat = permute(repmat(val_yt, [1,1,1,size(poly_chebyshev,2)]),[1,2,4,3]);
+    %visualize it
+    % plot_heatmap(val_xt_repmat)
+    % plot_heatmap(val_yt_repmat)
 
     %size of phi: NUM_GRID_PTS x NUM_GRID_PTS x MAX_DEGREE x MAX_DEGREE
     %size of W: MAX_DEGREE x MAX_DEGREE x NUM_DIMS x (NUM_DIMS+EXTRA_DIMS)
-    phi = val_xxt.*val_xxv;
+    phi = val_xt_repmat.*val_yt_repmat;
+    
     %equivalent of np.einsum(ij,jk-ik',A,B)
-    U = tensorprod(phi,W,[3,4],[1,2]);
+    if ~isempty(W)
+        U = tensorprod(phi,W,[3,4],[1,2]); %w is eta in some of the equations
+    else
+        U = [];
+    end
 end
 
 function prob = predict_error_prob(x, xbar, Basis_chebyshev,W_true, etas)
@@ -284,6 +239,20 @@ function prob = predict_error_prob(x, xbar, Basis_chebyshev,W_true, etas)
             p = mvnpdf(squeeze(z(t,v,:,:)), squeeze(x(t,v,:))', squeeze(S(t,v,:,:)));
             q = mvnpdf(squeeze(z(t,v,:,:)), squeeze(xbar(t,v,:))', squeeze(Sbar(t,v,:,:)));
             prob(t,v) = mean(min(p,q)./(p+q));
+        end
+    end
+end
+
+function plot_heatmap(M)
+    nRows = size(M,3);
+    nCols = size(M,4);
+    figure
+    for r = 1:nRows
+        for c = 1:nCols
+            colormap("summer")
+            subplot(nRows, nCols, c+nCols*(r-1))
+            imagesc(M(:,:,r,c));
+            xticks([]); yticks([]); axis square;
         end
     end
 end
