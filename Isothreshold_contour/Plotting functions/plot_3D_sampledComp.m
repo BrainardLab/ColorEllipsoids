bf02615da0498141e732d5ac3351b3a7ba6fb74e
FangfangHong <fh862@nyu.edu>
2024-03-04 23:57:37 -0500

    ..

--- Isothreshold_contour/Plotting functions/plot_3D_sampledComp.m
+++ Isothreshold_contour/Plotting functions/plot_3D_sampledComp.m
@@ -6,11 +6,14 @@ function plot_3D_sampledComp(ref_points, fitEllipsoid_unscaled, sampledComp,...
     p.addParameter('visualize_ellipsoid', true, @islogical);
     p.addParameter('visualize_samples', true, @islogical);
     p.addParameter('visualize_ellipse', false, @islogical);
+    p.addParameter('visualize_ellipse_export_from_2Dsim', false, @islogical);
+    p.addParameter('ellipse_2Dsim', [],@(x)(isnumeric(x)));
     p.addParameter('slc_grid_ref_dim1',1:length(ref_points),@(x)(isnumeric(x)));
     p.addParameter('slc_grid_ref_dim2',1:length(ref_points),@(x)(isnumeric(x)));
     p.addParameter('surf_alpha',0.3,@isnumeric);
     p.addParameter('samples_alpha',0.5,@isnumeric);
     p.addParameter('lineWidth_ellipse', 2, @isnumeric);
+    p.addParameter('lineWidth_ellipse_2Dsim',1, @isnumeric);
     p.addParameter('markerSize_samples',10,@isnumeric);
     p.addParameter('default_viewing_angle',true,@islogical);
     p.addParameter('fontsize',15,@isnumeric);
@@ -25,11 +28,14 @@ function plot_3D_sampledComp(ref_points, fitEllipsoid_unscaled, sampledComp,...
     visualize_ellipsoid = p.Results.visualize_ellipsoid;
     visualize_samples   = p.Results.visualize_samples;
     visualize_ellipse   = p.Results.visualize_ellipse;
+    visualize_ellipse_2Dsim = p.Results.visualize_ellipse_export_from_2Dsim;
+    ellipse_2Dsim       = p.Results.ellipse_2Dsim;
     slc_grid_ref_dim1   = p.Results.slc_grid_ref_dim1;
     slc_grid_ref_dim2   = p.Results.slc_grid_ref_dim2;
     surf_alpha          = p.Results.surf_alpha;
     samples_alpha       = p.Results.samples_alpha;
     lw_ellipse          = p.Results.lineWidth_ellipse;
+    lw_ellipse_2Dsim    = p.Results.lineWidth_ellipse_2Dsim;
     markerSize_samples  = p.Results.markerSize_samples;
     default_viewing_angle = p.Results.default_viewing_angle;
     fontsize   = p.Results.fontsize;
@@ -58,7 +64,7 @@ function plot_3D_sampledComp(ref_points, fitEllipsoid_unscaled, sampledComp,...
     tiledlayout(nGridPts_dim2,nGridPts_dim1,'TileSpacing','none');
     for j = nGridPts_dim2:-1:1
         jj = ref_points_idx(slc_grid_ref_dim2(j));
-        for i = 1:1:nGridPts_dim1
+        for i = 1:nGridPts_dim1
             ii = ref_points_idx(slc_grid_ref_dim1(i));
             nexttile
         
@@ -70,6 +76,9 @@ function plot_3D_sampledComp(ref_points, fitEllipsoid_unscaled, sampledComp,...
                 slc_gt_z = reshape(slc_gt(:,3), [nPhiEllipse, nThetaEllipse]);
                 slc_rgb_comp = squeeze(sampledComp(idx_x, ii,jj,:,:));
                 slc_ellipse = squeeze(ellipse_2D(idx_x, ii,jj,1,:,:));
+                if ~isempty(ellipse_2Dsim)
+                    slc_ellipse_2Dsim = squeeze(ellipse_2Dsim(idx_x, ii,jj,1,:,:));
+                end
             elseif strcmp(fixedPlane, 'G')
                 slc_ref = [ref_points(ii),fixedPlaneVal, ref_points(jj)];
                 slc_gt = squeeze(fitEllipsoid_unscaled(ii, idx_y, jj,:,:));
@@ -78,6 +87,9 @@ function plot_3D_sampledComp(ref_points, fitEllipsoid_unscaled, sampledComp,...
                 slc_gt_z = reshape(slc_gt(:,3), [nPhiEllipse, nThetaEllipse]);
                 slc_rgb_comp = squeeze(sampledComp(ii, idx_y, jj,:,:));    
                 slc_ellipse = squeeze(ellipse_2D(ii,idx_y,jj,2,:,:));
+                if ~isempty(ellipse_2Dsim)
+                    slc_ellipse_2Dsim = squeeze(ellipse_2Dsim(ii,idx_y,jj,2,:,:));
+                end
             elseif strcmp(fixedPlane,'B')
                 slc_ref = [ref_points(ii),ref_points(jj),fixedPlaneVal];
                 slc_gt = squeeze(fitEllipsoid_unscaled(ii, jj, idx_z,:,:));
@@ -86,6 +98,9 @@ function plot_3D_sampledComp(ref_points, fitEllipsoid_unscaled, sampledComp,...
                 slc_gt_z = reshape(slc_gt(:,3), [nPhiEllipse, nThetaEllipse]);
                 slc_rgb_comp = squeeze(sampledComp(ii,jj,idx_z,:,:));       
                 slc_ellipse = squeeze(ellipse_2D(ii,jj,idx_z,3,:,:));
+                if ~isempty(ellipse_2Dsim)
+                    slc_ellipse_2Dsim = squeeze(ellipse_2Dsim(ii,jj,idx_z,3,:,:));
+                end
             end
 
             hold on
@@ -97,14 +112,7 @@ function plot_3D_sampledComp(ref_points, fitEllipsoid_unscaled, sampledComp,...
                 camlight headlight; lighting phong;
             end
 
-            % Plot the 3D sampled points if enabled
-            if visualize_samples
-                scatter3(slc_rgb_comp(1,:), slc_rgb_comp(2,:), slc_rgb_comp(3,:),...
-                    markerSize_samples, 'MarkerFaceColor', [0,0,0],...
-                    'MarkerEdgeColor','none', 'MarkerFaceAlpha', samples_alpha);
-            end
-
-            % Plot the 2D ellipse if enabled and applicable
+            % Plot the 2D ellipse (derived from 3D simulations) if enabled and applicable
             if visualize_ellipse
                 if strcmp(fixedPlane,'R')
                     h2 = plot3(slc_ref(1).*ones(size(slc_ellipse)), slc_ellipse(:,1), ...
@@ -118,6 +126,32 @@ function plot_3D_sampledComp(ref_points, fitEllipsoid_unscaled, sampledComp,...
                         'Color',slc_ref,'LineWidth',lw_ellipse);                     
                 end
             end
+            hold on
+            % Plot the 2D ellipse (derived from 2D simulations)
+            if visualize_ellipse_2Dsim
+                if strcmp(fixedPlane,'R')
+                    h3 = plot3(slc_ref(1).*ones(size(slc_ellipse_2Dsim)),...
+                        slc_ellipse_2Dsim(:,1), slc_ellipse_2Dsim(:,2),...
+                        'Color','w','LineWidth',lw_ellipse_2Dsim,'LineStyle','--');         
+                elseif strcmp(fixedPlane,'G')
+                    h3 = plot3(slc_ellipse_2Dsim(:,1), ...
+                        slc_ref(2).*ones(size(slc_ellipse_2Dsim)),...
+                        slc_ellipse_2Dsim(:,2), 'Color','w','LineStyle','--',...
+                        'LineWidth',lw_ellipse_2Dsim); 
+                else
+                    h3 = plot3(slc_ellipse_2Dsim(:,1),  slc_ellipse_2Dsim(:,2), ...
+                        slc_ref(3).*ones(size(slc_ellipse_2Dsim)),...
+                        'Color','w','LineWidth',lw_ellipse_2Dsim,'LineStyle','--');                     
+                end
+            end
+
+            % Plot the 3D sampled points if enabled
+            if visualize_samples
+                scatter3(slc_rgb_comp(1,:), slc_rgb_comp(2,:), slc_rgb_comp(3,:),...
+                    markerSize_samples, 'MarkerFaceColor', [0,0,0],...
+                    'MarkerEdgeColor','none', 'MarkerFaceAlpha', samples_alpha);
+            end
+
             
             % Setting the axes limits and appearance
             xlim(slc_ref(1)+[-0.025,0.025]);
@@ -140,12 +174,12 @@ function plot_3D_sampledComp(ref_points, fitEllipsoid_unscaled, sampledComp,...
             if j == 1 && i == nGridPts_dim1
                 if exist("h1","var") && exist("h2","var")
                     legend_list = {'Ground truth ellipsoid',...
-                        sprintf(['Ground truth ellipse\n with fixed',...
-                        fixedPlane,' = ',num2str(fixedPlaneVal)])};
+                        ['Ground truth ellipse with fixed ',...
+                        fixedPlane,' = ',num2str(fixedPlaneVal)]};
                 else
                     if ~exist("h1","var")
-                        legend_list = {sprintf(['Ground truth ellipse\n with fixed ',...
-                            fixedPlane,' = ',num2str(fixedPlaneVal)])};
+                        legend_list = {['Ground truth ellipse with fixed ',...
+                            fixedPlane,' = ',num2str(fixedPlaneVal)]};
                     else; legend_list = {'Ground truth ellipsoid'};
                     end
                 end
