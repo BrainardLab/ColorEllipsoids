b1270e51e55d92fc864647270e2e0d06b30f287d
FangfangHong <fh862@nyu.edu>
2025-03-17 17:37:35 -0400

    updated the script that fits the W model to simulated data based on CIELAB

--- Python version/data_reorg.py
+++ Python version/data_reorg.py
@@ -22,7 +22,9 @@ def organize_data(ndims, sim, x1_scaler, **kwargs):
     params = {
         'slc_idx':[], #if we do not want to include all the ref stimuli, we can select a subset, e.g., [0, 2, 4]
         'visualize_samples':False,
-        'plane_2D':''
+        'plane_2D':'',
+        'flag_convert_to_W': True,
+        'M_2DWToRGB': None,
     }
     # Update default parameters with any additional keyword arguments provided
     params.update(kwargs)
@@ -34,7 +36,8 @@ def organize_data(ndims, sim, x1_scaler, **kwargs):
         #the comparison stimulus was sampled around 5 x 5 different ref stimuli
         #the original data ranges from 0 to 1; we scale it so that it fits the [-1, 1] cube
         x1_raw = sim['rgb_comp'][idx][:,idx][:,:,sim['varying_RGBplane'],:]
-        x1_raw = color_thresholds.N_unit_to_W_unit(x1_raw)
+        if params['flag_convert_to_W']:
+            x1_raw = color_thresholds.N_unit_to_W_unit(x1_raw)
         #number of reference stimuli (5,5)
         ref_size_dim1, ref_size_dim2 = x1_raw.shape[0: ndims]
         #reshape the array so the final size is (2000, 3)
@@ -42,7 +45,8 @@ def organize_data(ndims, sim, x1_scaler, **kwargs):
     
         #reference stimulus
         xref_raw            = sim['ref_points'][sim['varying_RGBplane']][:,idx][:,:,idx]
-        xref_raw = color_thresholds.N_unit_to_W_unit(xref_raw)
+        if params['flag_convert_to_W']:
+            xref_raw = color_thresholds.N_unit_to_W_unit(xref_raw)
         xref_temp_expanded  = np.expand_dims(np.transpose(xref_raw, (1,2,0)), axis=-1)
         xref_repeated       = np.tile(xref_temp_expanded, (1, 1, 1, sim['nSims']))
         xref_temp_reshaped  = np.stack((xref_repeated[:,:,0].ravel(),\
@@ -52,11 +56,13 @@ def organize_data(ndims, sim, x1_scaler, **kwargs):
         
     elif ndims == 3:
         x1_raw = sim['rgb_comp'][idx][:,idx][:,:,idx][:,:,:,sim['varying_RGBplane']] 
-        x1_raw = color_thresholds.N_unit_to_W_unit(x1_raw)
+        if params['flag_convert_to_W']:
+            x1_raw = color_thresholds.N_unit_to_W_unit(x1_raw)
         ref_size_dim1, ref_size_dim2, ref_size_dim3 = x1_raw.shape[0:ndims]
         x1_temp_reshaped    = np.transpose(x1_raw, (0,1,2,4,3)).reshape(-1, ndims)
         xref_raw            = sim['ref_points'][idx][:,idx][:,:,idx][:,:,:,sim['varying_RGBplane']]
-        xref_raw = color_thresholds.N_unit_to_W_unit(xref_raw)
+        if params['flag_convert_to_W']:
+            xref_raw = color_thresholds.N_unit_to_W_unit(xref_raw)
         xref_temp_expanded  = np.expand_dims(xref_raw, axis=-1)
         xref_repeated       = np.tile(xref_temp_expanded, (1, 1, 1,1, sim['nSims']))
         xref_temp_reshaped  = np.stack((xref_repeated[:,:,:,0].ravel(),\
@@ -86,8 +92,12 @@ def organize_data(ndims, sim, x1_scaler, **kwargs):
             i_lb = i*sim['nSims']
             i_ub = (i+1)*sim['nSims']
             #define the color map, which is the RGB value of the reference stimulus
-            cm = (xref_jnp[i_lb] + 1)/2
-            cm = np.insert(cm, sim['slc_RGBplane'], 0.5);
+            if params['flag_convert_to_W']:
+                cm = color_thresholds.W_unit_to_N_unit(xref_jnp[i_lb])
+                cm = np.insert(cm, sim['slc_RGBplane'], 0.5)
+            else:
+                cm = np.insert(xref_jnp[i_lb], sim['slc_RGBplane'], 1)
+                cm = params['M_2DWToRGB'] @ cm
             ax[0].scatter(x1_jnp[i_lb:i_ub,0], x1_jnp[i_lb:i_ub,1], c = cm,s = 1, alpha = 0.5)
             ax[0].scatter(xref_jnp[i_lb:i_ub,0], xref_jnp[i_lb:i_ub,1],\
                           c = cm,s = 30,marker = '+')
@@ -99,9 +109,12 @@ def organize_data(ndims, sim, x1_scaler, **kwargs):
         ax[0].set_xlim([-1, 1]); ax[0].set_ylim([-1, 1])
         ax[0].grid(True, alpha=0.3)
         ax[0].set_xlim([-1, 1]); ax[0].set_ylim([-1, 1])
-        if params['plane_2D'] != '':
+        if params['plane_2D'] in ['GB plane', 'RG plane', 'RB plane']:
             ax[0].set_xlabel(params['plane_2D'][0]);
             ax[0].set_ylabel(params['plane_2D'][1])
+        else: #suggest this is the isoluminant plane
+            ax[0].set_xlabel('Wishart space dimension 1')
+            ax[0].set_ylabel('Wishart space dimension 2')
         ax[0].set_title('Comparison stimuli')
         ax[1].set_title('Comparison - Reference')
         ax[1].grid(True, alpha=0.3)
--- Python version/fit_2d_CIELab.py
+++ Python version/fit_2d_CIELab.py
@@ -9,15 +9,15 @@ This fits a Wishart Process model to the simulated data using the CIELab color s
 
 """
 
-#%% import modules
+#import modules
 import jax
 jax.config.update("jax_enable_x64", True)
 import jax.numpy as jnp
 import matplotlib.pyplot as plt
 import dill as pickle
 import sys
+import os
 import numpy as np
-
 sys.path.append("/Users/fangfang/Documents/MATLAB/projects/ellipsoids/ellipsoids")
 from core import optim, oddity_task
 from core.wishart_process import WishartProcessModel
@@ -26,189 +26,223 @@ from core.model_predictions import wishart_model_pred
 from plotting.wishart_predictions_plotting import WishartPredictionsVisualization
 sys.path.append('/Users/fangfang/Documents/MATLAB/projects/ColorEllipsoids/Python version')
 from data_reorg import organize_data
+from analysis.cross_validation import expt_data
 
 #%% three variables we need to define for loading the data
-plane_2D      = 'RG plane'
-plane_2D_dict = {'GB plane': 0, 'RB plane': 1, 'RG plane': 2}
-plane_2D_idx  = plane_2D_dict[plane_2D]
-sim_jitter    = '0.3'
-nSims         = 160 #number of simulations: 240 trials for each ref stimulus
+#plane_2D      = 'RG plane'
+#plane_2D_dict = {'GB plane': 0, 'RB plane': 1, 'RG plane': 2}
+#plane_2D_idx  = plane_2D_dict[plane_2D]
+plane_2D      ='Isoluminant plane'
+plane_2D_idx  = 2
+sim_jitter    = 0.3
+nSims         = 240 #number of simulations: 
+rnd_seed      = 0
 
-for rr in range(10):
-    rnd_seed      = rr
+baseDir = '/Volumes/T9/Aguirre-Brainard Lab Dropbox/Fangfang Hong/'
+output_figDir_fits = os.path.join(baseDir, 'ELPS_analysis','ModelFitting_FigFiles',
+                                  'Python_version','2D_oddity_task', plane_2D)
+output_fileDir = os.path.join(baseDir, 'ELPS_analysis','ModelFitting_DataFiles',
+                              '2D_oddity_task', plane_2D)
+
+#%%
+# -----------------------------------------------------------
+# Load data simulated using CIELab and organize data
+# -----------------------------------------------------------
+#file 1
+path_str = os.path.join(baseDir, 'ELPS_analysis','Simulation_DataFiles')
+if plane_2D == 'Isoluminant plane':
+    colordiff_alg = 'CIE2000'
+    scaler_x1 = 2.5
+    flag_convert_to_W = False
+else:
+    colordiff_alg = ''
+    scaler_x1 = 5
+    flag_convert_to_W = True 
+file_sim = f"Sims_isothreshold_{plane_2D}_{colordiff_alg}_sim{nSims}perCond_"+\
+            f"samplingNearContour_jitter{sim_jitter}_seed{rnd_seed}.pkl"
+full_path = f"{path_str}/{plane_2D}/{file_sim}"   
+with open(full_path, 'rb') as f:  
+    data_load = pickle.load(f)
+sim = data_load[0]
+
+# Create an instance of the class
+color_thres_data = color_thresholds(2, baseDir + 'ELPS_analysis/',
+                                    plane_2D = plane_2D)
+# Load Wishart model fits
+color_thres_data.load_CIE_data(CIE_version = colordiff_alg)
+color_thres_data.load_transformation_matrix()
+
+#organize simulated data to jnp
+data_temp, x1_raw, xref_raw = organize_data(2, sim, scaler_x1,
+                                       visualize_samples = True,
+                                       flag_convert_to_W = flag_convert_to_W,
+                                       plane_2D = plane_2D,
+                                       M_2DWToRGB = color_thres_data.M_2DWToRGB)
+y_jnp, xref_jnp, x0_jnp, x1_jnp = data_temp
+data = (y_jnp, xref_jnp, x1_jnp)
+
+#%% 
+# -------------------------------
+# Constants describing simulation
+# -------------------------------
+model = WishartProcessModel(
+    5,     # Degree of the polynomial basis functions
+    2,     # Number of stimulus dimensions
+    1,     # Number of extra inner dimensions in `U`.
+    3e-4,  # Scale parameter for prior on `W`.
+    0.4,   # Geometric decay rate on `W`. 
+    0,     # Diagonal term setting minimum variance for the ellipsoids.
+)
+
+NUM_GRID_PTS = 5      # Number of grid points over stimulus space.
+MC_SAMPLES   = 2000        # Number of simulated trials to compute likelihood.
+BANDWIDTH    = 5e-3        # Bandwidth for logistic density function.
+opt_params = {
+    "learning_rate": 1e-4,
+    "momentum": 0.2,
+    "mc_samples": MC_SAMPLES,
+    "bandwidth": BANDWIDTH,
+}
+
+# -----------------------------
+# Fit W by maximizing posterior
+# -----------------------------
+# Search for the best-fit parameters from three random initializations to avoid
+# getting stuck at local minimum
+nRepeats = 3
+#Generate a matrix of random seeds for each initialization
+random_seeds = np.random.randint(0, 2**32, size = (nRepeats, 2))
+# Initialize a high upper bound for negative log-likelihood (nLL) to track the best fit
+objhist_end = 1e3  # Start with a large number to ensure any valid fit is better
+# Loop through each random initialization
+for i in range(nRepeats):
+    # Generate random keys for initializing parameters, data, and optimizer
+    W_INIT_KEY_i   = jax.random.PRNGKey(random_seeds[i,0])  # Key to initialize `W_est`. 
+    OPT_KEY_i      = jax.random.PRNGKey(random_seeds[i,1])  # Key passed to optimizer.
     
-    baseDir = '/Volumes/T9/Aguirre-Brainard Lab Dropbox/Fangfang Hong/'
-    output_figDir_fits = baseDir + 'ELPS_analysis/ModelFitting_FigFiles/Python_version/2D_oddity_task/'
-    output_fileDir = baseDir + 'ELPS_analysis/ModelFitting_DataFiles/2D_oddity_task/'
+    # Fit model, initialized at a random W sampled from the prior distribution
+    W_init_i = model.sample_W_prior(W_INIT_KEY_i) 
     
-    #%
-    # -----------------------------------------------------------
-    # Load data simulated using CIELab and organize data
-    # -----------------------------------------------------------
-    #file 1
-    path_str      = baseDir + 'ELPS_analysis/Simulation_DataFiles/'
-    # Create an instance of the class
-    color_thres_data = color_thresholds(2, baseDir + 'ELPS_analysis/',
-                                        plane_2D = plane_2D)
-    # Load Wishart model fits
-    color_thres_data.load_CIE_data()
-    results = color_thres_data.get_data('results2D', dataset = 'CIE_data')  
-    
-    #file 2
-    file_sim      = f"Sims_isothreshold_{plane_2D}_sim{nSims}perCond_"+\
-                    f"samplingNearContour_jitter{sim_jitter}_seed{rnd_seed}.pkl"
-    full_path     = f"{path_str}{plane_2D}/{nSims} trials/{file_sim}"   
-    with open(full_path, 'rb') as f:  data_load = pickle.load(f)
-    sim = data_load[0]
-    
-    """
-    Fitting would be easier if we first scale things up, and then scale the model 
-    predictions back down
-    """
-    scaler_x1  = 5
-    data, x1_raw, xref_raw = organize_data(2, sim, scaler_x1,
-                                           visualize_samples = True,
-                                           plane_2D = plane_2D)
-    ref_size_dim1, ref_size_dim2 = x1_raw.shape[0:2]
-    y_jnp, xref_jnp, x0_jnp, x1_jnp = data 
-    data_new = (y_jnp, xref_jnp, x1_jnp)
-    
-    #% -------------------------------
-    # Constants describing simulation
-    # -------------------------------
-    model = WishartProcessModel(
-        5,     # Degree of the polynomial basis functions
-        2,     # Number of stimulus dimensions
-        1,     # Number of extra inner dimensions in `U`.
-        3e-4,  # Scale parameter for prior on `W`.
-        0.4,   # Geometric decay rate on `W`. 
-        0,     # Diagonal term setting minimum variance for the ellipsoids.
-    )
-    
-    NUM_GRID_PTS = 5      # Number of grid points over stimulus space.
-    MC_SAMPLES   = 2000        # Number of simulated trials to compute likelihood.
-    BANDWIDTH    = 5e-3        # Bandwidth for logistic density function.
-    
-    # Random number generator seeds
-    W_INIT_KEY   = jax.random.PRNGKey(227)  # Key to initialize `W_est`. 227
-    DATA_KEY     = jax.random.PRNGKey(333)  # Key to generate datatset.
-    OPT_KEY      = jax.random.PRNGKey(444)  # Key passed to optimizer.
-    
-    # -----------------------------
-    # Fit W by maximizing posterior
-    # -----------------------------
-    # Fit model, initialized at random W
-    W_init = 1e-1*model.sample_W_prior(W_INIT_KEY) 
-    
-    opt_params = {
-        "learning_rate": 1e-3,#1e-2
-        "momentum": 0.2,
-        "mc_samples": MC_SAMPLES,
-        "bandwidth": BANDWIDTH,
-    }
-    W_est, iters, objhist = optim.optimize_posterior(
-        W_init, data_new, model, OPT_KEY,
+    W_est_i, iters_i, objhist_i = optim.optimize_posterior(
+        W_init_i, data, model, OPT_KEY_i,
         opt_params,
-        oddity_task.simulate_oddity, #oddity_task.simulate_oddity or oddity_task.simulate_oddity_reference
-        total_steps=500,
+        oddity_task.simulate_oddity, 
+        total_steps=1000,
         save_every=1,
         show_progress=True
     )
     
-    fig, ax = plt.subplots(1, 1)
-    ax.plot(iters, objhist)
-    fig.tight_layout()
-    
-    #%
-    # -----------------------------
-    # Rocover covariance matrices
-    # -----------------------------
-    # Specify grid over stimulus space
-    grid_1d = jnp.linspace(jnp.min(xref_jnp), jnp.max(xref_jnp), NUM_GRID_PTS)
-    grid = jnp.stack(jnp.meshgrid(*[grid_1d for _ in range(model.num_dims)]), axis=-1)
-    Sigmas_est_grid = model.compute_Sigmas(model.compute_U(W_est, grid))
-    
-    # -----------------------------
-    # Compute model predictions
-    # -----------------------------
-    model_pred_Wishart = wishart_model_pred(model, opt_params, W_INIT_KEY,
-                                            DATA_KEY, OPT_KEY, W_init, 
-                                            W_est, Sigmas_est_grid, 
-                                            color_thres_data,
-                                            target_pC=2/3,
-                                            scaler_x1 = scaler_x1,
-                                            ngrid_bruteforce = 500,
-                                            bds_bruteforce = [0.01, 0.25])
-    
-    grid_trans = np.transpose(grid,(2,0,1))
-    model_pred_Wishart.convert_Sig_Threshold_oddity_batch(grid_trans)
-    
-            
-    #%
-    # -----------------------------
-    # Visualize model predictions
-    # -----------------------------
-    #ground truth ellipses
+    # Update the best-fit model if the current fit improves the objective (lower nLL)
+    if objhist_i[-1] < objhist_end:
+        objhist_end = objhist_i[-1]
+        W_init, W_est, iters, objhist = W_init_i, W_est_i, iters_i, objhist_i
+        W_INIT_KEY, OPT_KEY = W_INIT_KEY_i, OPT_KEY_i
+        bestfit_seed = random_seeds[i]
+        
+fig, ax = plt.subplots(1, 1)
+ax.plot(iters, objhist)
+fig.tight_layout(); plt.show()
+
+#%%
+# -----------------------------
+# Compute model predictions
+# -----------------------------
+# Specify grid over stimulus space
+grid_1d = jnp.linspace(jnp.min(xref_jnp), jnp.max(xref_jnp), NUM_GRID_PTS)
+grid = jnp.stack(jnp.meshgrid(*[grid_1d for _ in range(model.num_dims)]), axis=-1)
+Sigmas_est_grid = model.compute_Sigmas(model.compute_U(W_est, grid))
+
+target_pC = 0.667
+model_pred_Wishart = wishart_model_pred(model, opt_params, W_INIT_KEY,
+                                        OPT_KEY, W_init, 
+                                        W_est, Sigmas_est_grid, 
+                                        color_thres_data,
+                                        scaler_x1 = scaler_x1,
+                                        target_pC=target_pC,
+                                        colordiff_alg = colordiff_alg,
+                                        ngrid_bruteforce = 1000,
+                                        bds_bruteforce = [0.0005, 0.25])
+
+grid_trans = np.transpose(grid,(2,0,1))
+model_pred_Wishart.convert_Sig_Threshold_oddity_batch(grid_trans)
+
+        
+#%%
+# -----------------------------
+# Visualize model predictions
+# -----------------------------
+#ground truth ellipses
+results = color_thres_data.get_data('results2D', dataset = 'CIE_data')  
+if not flag_convert_to_W:
+    gt_covMat_CIE = results['fitEllipse_scaled'][plane_2D_idx]
+else:
     gt_covMat_CIE = color_thresholds.N_unit_to_W_unit(results['fitEllipse_scaled'][plane_2D_idx])
-    #specify figure name and path
-    fig_name_part1 = 'Fitted' + file_sim[4:-4] + '_bandwidth' + str(BANDWIDTH)
-    
-    class sim_data:
-        def __init__(self, xref_all, x1_all):
-            self.xref_all = xref_all
-            self.x1_all = x1_all
-    sim_trial_by_CIE = sim_data(xref_jnp, x1_jnp)
-    
-    wishart_pred_vis = WishartPredictionsVisualization(sim_trial_by_CIE,
-                                                         model, 
-                                                         model_pred_Wishart, 
-                                                         color_thres_data,
-                                                         fig_dir = output_figDir_fits, 
-                                                         save_fig = False)
-            
-    wishart_pred_vis.plot_2D(
-        grid, 
-        grid,
-        gt_covMat_CIE, 
-        visualize_samples= True,
-        visualize_gt = False,
-        visualize_model_estimatedCov = True,
-        samples_alpha = 1,
-        samples_s = 1,
-        plane_2D = plane_2D,
-        modelpred_ls = '-',
-        modelpred_lc = [0.3,0.3,0.3],
-        modelpred_lw = 2,
-        modelpred_alpha = 1,
-        gt_lw= 0.5,
-        gt_lc =[0.1,0.1,0.1],
-        fig_name = fig_name_part1+'_withSamples.pdf') 
-    
-    wishart_pred_vis.plot_2D(
-        grid, 
-        grid,
-        gt_covMat_CIE, 
-        visualize_samples= False,
-        visualize_model_estimatedCov= False,
-        visualize_gt = True,
-        gt_lw = 1, 
-        gt_alpha = 0.5, 
-        modelpred_lw = 2, 
-        modelpred_alpha = 0.5,
-        fig_name = fig_name_part1 + '.pdf') 
-    
-            
-    #% save data
-    output_file = fig_name_part1 + "_oddity.pkl"
-    full_path = f"{output_fileDir}{output_file}"
-    
-    variable_names = ['plane_2D', 'sim_jitter','nSims', 'data','x1_raw',
-                      'xref_raw','sim_trial_by_CIE', 'grid_1d', 'grid','grid_trans',
-                      'iters', 'objhist','model','model_pred_Wishart', 'gt_covMat_CIE']
-    vars_dict = {}
-    for i in variable_names: vars_dict[i] = eval(i)
-    
-    # Write the list of dictionaries to a file using pickle
-    with open(full_path, 'wb') as f:
-        pickle.dump(vars_dict, f)
-    
+#specify figure name and path
+fig_name_part1 = f'Fitted{file_sim[4:-4]}_bandwidth{BANDWIDTH}'
+
+sim_trial_by_CIE = expt_data(xref_jnp, x1_jnp, y_jnp, None)
+wishart_pred_vis = WishartPredictionsVisualization(sim_trial_by_CIE,
+                                                   model, 
+                                                   model_pred_Wishart, 
+                                                   color_thres_data,
+                                                   fig_dir = output_figDir_fits, 
+                                                   save_fig = True)
+        
+wishart_pred_vis.plot_2D(
+    grid, 
+    grid,
+    gt_covMat_CIE, 
+    visualize_samples= True,
+    visualize_gt = False,
+    visualize_model_estimatedCov = True,
+    flag_rescale_axes_label = False,
+    samples_alpha = 0.75,
+    samples_s = 1,
+    plane_2D = plane_2D,
+    modelpred_ls = '-',
+    modelpred_lc = [0,0,0],
+    modelpred_lw = 0.5,
+    modelpred_alpha = 1,
+    fontsize = 9,
+    fig_name = f"{fig_name_part1}_withSamples.pdf") 
+
+wishart_pred_vis.plot_2D(
+    grid, 
+    grid,
+    gt_covMat_CIE, 
+    visualize_samples= False,
+    visualize_model_estimatedCov= False,
+    visualize_gt = True,
+    gt_lw = 1, 
+    gt_alpha = 0.5, 
+    modelpred_lc = 'g',
+    modelpred_lw = 1.5, 
+    modelpred_alpha = 0.5,
+    fontsize = 9,
+    fig_name = f"{fig_name_part1}.pdf") 
+
+        
+#%% save data
+output_file = f"{fig_name_part1}_decay{model.decay_rate}_oddity.pkl"
+full_path = f"{output_fileDir}/{output_file}"
+
+variable_names = ['plane_2D','sim_jitter','nSims','rnd_seed','colordiff_alg',
+                  'scaler_x1', 'file_sim', 'sim', 'color_thres_data', 'data',
+                  'x1_raw', 'xref_raw','model','opt_params','nRepeats',
+                  'random_seeds', 'objhist_end', 'bestfit_seed', 
+                  'model','model_pred_Wishart', 'sim_trial_by_CIE', 
+                  'grid','grid_trans','gt_covMat_CIE']
+vars_dict = {}
+for var_name in variable_names:
+    try:
+        # Check if the variable exists in the global scope
+        vars_dict[var_name] = eval(var_name)
+    except NameError:
+        # If the variable does not exist, assign None and print a message
+        vars_dict[var_name] = None
+        print(f"Variable '{var_name}' does not exist. Assigned as None.")
+
+# Write the list of dictionaries to a file using pickle
+with open(full_path, 'wb') as f:
+    pickle.dump(vars_dict, f)
+
